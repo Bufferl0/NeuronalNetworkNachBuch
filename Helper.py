28637af1aa05608cd82ad43ee2962353b710e2e1
Jazit <jazi0006@stud.hs-kl.de>
2019-09-25 11:24:29 +0200

    0.1.2, Ordner wird angelegt falls nicht existiert

--- Helper.py
+++ Helper.py
@@ -4,6 +4,7 @@ import os
 
 # --Created by Janek Zitzmann, 17.09.2019
 class pngWandler:
+    """Öfnet alle Bilder im angegeben Pfad und wandelt sie in ein 1D Array um"""
     def __init__(self, path):
         self.path = path
 
--- Main.py
+++ Main.py
@@ -7,19 +7,7 @@ path = "C:/pythonImg/"
 """=======================================================CODE======================================================="""
 #eingabe = Eingabe(path)
 erg = pngWandler(path).openPictures()
-np.set_printoptions(threshold=np.inf)
+np.set_printoptions(threshold=np.inf) # wird für Ausgabe der Matrizen benötigt
+eing = Eingabe(path)
 
-print(erg)
-nullen = 0
-einsen = 0
-if erg is None:
-    print("erst Bilder anlegen")
-else:
-    for a in erg[1]:
-        if a < 1.0:
-            nullen += 1
-        else:
-            einsen += 1
-    print(nullen)
-    print(einsen)
 
